c0aed015fb70fb83542aaa277ff821c2f5a8b077
BrandonStock1 <46919113@est.ort.edu.ar>
2022-03-29 10:42:11 -0300

    Exercise 1 y 2 finished

--- Assets/Scripts/Ejercicio1.cs
+++ Assets/Scripts/Ejercicio1.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Ejercicio1 : MonoBehaviour
 {
+    // 1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente  valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el  resultado de multiplicarlo por sí mismo. //
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int num1;
+        num1 = 0;
+        num1 = num1 + 2;
+        Debug.Log("El resultado de aumentar en 2 el número ingresado, es " + num1);
+        num1 = num1 * num1;
+        Debug.Log("El resultado de multiplicar por sí mismo el número ingresado aumentado en 2, es " + num1);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    //2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,  respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: //
+    // a.Suma //
+    // b.Producto //
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+        int res;
+        res = num1 + num2;
+        Debug.Log("El resultado de sumar el " + num1 + " y " + num2 + " ,es " + res);
+        res = num1 * num2;
+        Debug.Log("El resultado de multiplicar el " + num1 + " y " + num2 + " ,es " + res);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
